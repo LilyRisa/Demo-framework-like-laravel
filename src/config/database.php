0b5ca8286976ac78f5517d42145a1c8951685290
DESKTOP-AV76JFA\minhm <bvminh101299@gmail.com>
2022-05-16 19:43:48 +0700

    gitignore woring

--- src/config/database.php
+++
@@ -1,9 +0,0 @@
-
-<?php 
-$db = [
-    'host' => 'localhost',
-    'user' => 'root',
-    'password' => '',
-    'database' => 'congminh',
-    'port' => '3306',
-];
--- src/config/enviroment.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-$filename = ROOTPATH.'/src/logs/logs.txt';
-$datetimeFormat = 'd-m-Y';
-$cache_view = 'C:\xampp\htdocs\Test/src/Views/cache';
-$debug = 1;
-define('PATH_VIEW', __DIR__.'/../Views');
-
-    
